5c6d188b0c6075ed5d188cb5e87845fd922c0bf6
Thedduro <thedduro@Thedduroui-MacBookPro.local>
2025-08-09 20:16:09 +0900

    전기버스 문제 풀이

---
+++ algorithm/4831. 전기버스/solve.py
@@ -0,0 +1,39 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+def find(current_idx):
+    global result 
+    
+    while current_idx < N: # 현재 위치가 종점보다 작을 때
+        next_idx = current_idx + K
+
+        if next_idx >= N:
+            break
+        
+        found = False
+        for i in range(next_idx, current_idx, -1):
+            if i in charge_stations:
+                next_idx = i
+                result += 1
+                found = True
+                break
+
+        if not found:
+            return 0
+        
+        current_idx = next_idx
+
+    return result 
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    K, N, M = map(int, input().split())
+
+    charge_stations = list(map(int, input().split()))
+
+    result = 0
+    current_idx = 0
+ 
+  
+    print(f'#{tc}', find(current_idx))
