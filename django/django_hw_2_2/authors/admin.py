1291251f7549d232d8237108ea9d012563913615
Thedduro <lsw2207@gmail.com>
2025-08-14 14:52:40 +0900

    3461.Django Serializers - SNS 1

--- django/django_hw_2_2/authors/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Author
-
-# Register your models here.
-
-# 관리자 사이트에 저자정보 등록
-admin.site.register(Author)
--- django/django_hw_2_2/authors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # 사용자가 PK에 대한 정보를 url에 보낼 예정
-    # 그때 경로는 variable routing'
-    path('<int:author_pk>/', views.author_detail)
-]
