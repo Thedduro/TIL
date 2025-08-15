dde310f4d34dc797fe3af7faaa927d74b125c779
Thedduro <thedduro@Thedduroui-MacBookPro.local>
2025-08-15 14:05:41 +0900

    2382. 미생물 격리

---
+++ algorithm/2382. 미생물 격리/fail_solve.py
@@ -0,0 +1,53 @@
+import sys
+sys.stdin = open('sample_input.txt','r')
+
+# 약품에 닿으면, 절반 감소 > 방향 반대로 바뀜
+# 수가 0이되면 군집이 사라짐
+# 군집 두개가 한셀에 모이면 군집이 합쳐짐, 이동방향은 군집이 더 큰 군집꺼
+# 시간이 지난 후 남아있는 미생물 수 총합
+
+direction = [(1,0),(-1,0),(0,-1),(0,1)]
+
+def merge(lst): 
+    '''
+    1시간마다 같은 셀에 있는 군집을 찾고 병합, 방향 수정해서 리턴한다.
+    그러면 모든 군집의 리스트를 받고 처리 > 리스트 반환
+    어떻게?
+    for문으로 하나씩 돌면서 안에 같은 위치가 있으면 병합 후 그 군집과 병합
+    '''
+    next_lst = []
+    for i in range(len(lst)):
+        x1, y1, cnt1, dir1= lst[i][0], lst[i][1], lst[i][2], lst[i][3]
+        if i == K:
+            next_lst.append([x1, y1, cnt1, dir1])
+        elif i < K:
+            for j in range(i+1, K-1):
+                x2, y2, cnt2, dir2 = lst[j][0], lst[j][1], lst[j][2], lst[j][3]
+                if x1==x2 and y1==y2: # 같을때
+                    cnt = cnt1 + cnt2
+                    if cnt1 < cnt2:
+                        dir = dir2
+                    else:
+                        dir = dir1
+                    next_lst.append([x1, y2, cnt, dir])
+                else: # 같은 경우가 없을땐?
+                    next_lst.append([x1, y1, cnt1, dir1])
+    return next_lst
+
+def solve(time):
+    while time < M:
+        for i in range(K):
+            x, y, cnt, dir = lst[i][0], lst[i][1], lst[i][2], lst[i][3]
+            nx, ny = x + direction[dir-1][0], y + direction[dir-1][1]
+            if nx == 
+            print(nx, ny)
+
+
+T = int(input())
+
+for tc in range(1, 11):
+    N, M, K = map(int, input().split())
+
+    lst = [list(map(int, input().split())) for _ in range(K)] # 세로 위치, 가로 위치, 미생물 수, 이동 방향
+
+    print(solve(1))
--- algorithm/2382. 미생물 격리/solve.py
+++ algorithm/2382. 미생물 격리/solve.py
@@ -1,47 +1,53 @@
 import sys
 sys.stdin = open('sample_input.txt','r')
 
-# 약품에 닿으면, 절반 감소 > 방향 반대로 바뀜
-# 수가 0이되면 군집이 사라짐
-# 군집 두개가 한셀에 모이면 군집이 합쳐짐, 이동방향은 군집이 더 큰 군집꺼
-# 시간이 지난 후 남아있는 미생물 수 총합
-
-direction = [(1,0),(-1,0),(0,-1),(0,1)]
+direction = [(-1,0),(1,0),(0,-1),(0,1)]
+reverse_dir = {1: 2, 2: 1, 3: 4, 4: 3}
 
 def merge(lst): 
-    '''
-    1시간마다 같은 셀에 있는 군집을 찾고 병합, 방향 수정해서 리턴한다.
-    그러면 모든 군집의 리스트를 받고 처리 > 리스트 반환
-    어떻게?
-    for문으로 하나씩 돌면서 안에 같은 위치가 있으면 병합 후 그 군집과 병합
-    '''
+    merged = {}
+
+    for x, y, cnt, dir in lst:
+        if (x,y) not in merged:
+            merged[(x,y)] = [(cnt,dir)]
+        else:
+            merged[(x,y)].append((cnt,dir))
+    
     next_lst = []
+    
+    for (x, y), value in merged.items():
+        if len(value) == 1:
+            next_lst.append([x, y, value[0][0], value[0][1]])
+        else:
+            cnt = sum(v[0] for v in value)
+            max_cnt, max_dir = max(value, key=lambda v: v[0])
+            next_lst.append([x,y,cnt,max_dir])
+    
+    return next_lst
 
-    for i in range(K-1):
-        x1, y1, cnt1, dir1= lst[i][0], lst[i][1], lst[i][2], lst[i][3]
-        for j in range(i+1, K):
-            x2, y2, cnt2, dir2 = lst[j][0], lst[j][1], lst[j][2], lst[j][3]
-            if x1==x2 and y1==y2:
-                cnt = cnt
-                if cnt1 < cnt2:
-                    dir = dir2
-                else:
-                    dir = dir1
-            next_lst.appned([x1, x2, cnt, dir])
+def solve(time, lst):
+    while time <= M:
+        for i, micro in enumerate(lst):
+            x, y = micro[0],micro[1]
+            cnt, dir = micro[2],micro[3]
+            nx , ny = x + direction[dir-1][0], y + direction[dir-1][1]
+            if nx == 0 or nx == N-1 or ny == 0 or ny == N-1: # 격리 구간에 닿으면,
+                cnt = cnt // 2
+                dir = reverse_dir[dir]
+            lst[i] = [nx, ny, cnt, dir]
 
-def solve(time):
-    while time < M:
-        for i in range(K):
-            x, y, cnt, dir = lst[i][0], lst[i][1], lst[i][2], lst[i][3]
-            nx, ny = x + direction[dir-1][0], y + direction[dir-1][1]
-            if nx == 
-            print(nx, ny)
+        lst = [micro for micro in lst if micro[2] > 0] # 미생물 수가 0일때, 제거
+        time += 1
+        lst = merge(lst)
+    return lst        
 
 T = int(input())
 
-for tc in range(1, 11):
+for tc in range(1, T+1):
     N, M, K = map(int, input().split())
-
     lst = [list(map(int, input().split())) for _ in range(K)] # 세로 위치, 가로 위치, 미생물 수, 이동 방향
-
-    print(solve(1))
+    
+    result_lst = solve(1,lst)
+    result = sum(x[2] for x in result_lst)
+    
+    print(f'#{tc}', result)
